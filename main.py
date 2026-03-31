d104ef07dd4e4f34289752410ac4cb85cfdbc936
Alex <aliniscollins112@gmail.com>
2026-03-31 14:31:27 -0700

    Add delete and filter method

--- main.py
+++ main.py
@@ -2,10 +2,17 @@ class ExpenseTracker:
     def __init__(self, expenses):
         self._expenses = expenses
 
+    def delete(self, item):
+        del self._expenses[item]
+    
+    def filter(self, threshold):
+        filtered = {key: value for key, value in self._expenses.items() if value > threshold}
+        return filtered
+
     @property
     def expenses(self):
         return self._expenses
     
-ExT = ExpenseTracker([5,10,14])
+# e1 = ExpenseTracker({"Rent": 1200, "Utilities": 400, "Food" : 600, "Necessities" : 100 })
 
-print(ExT.expenses)
+# print(e1.filter(500))
