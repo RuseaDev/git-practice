e25d0926bcb7362d769af9b4a252cb1ffee755a0
benn <192007>
2026-03-31 14:41:25 -0700

    add expense

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ class ExpenseTracker:
     def expenses(self):
         return self._expenses
     
+    def add(self, item):
+        return self._expenses.append(item)
+    
 ExT = ExpenseTracker([5,10,14])
 
+ExT.add(2)
 print(ExT.expenses)
+
